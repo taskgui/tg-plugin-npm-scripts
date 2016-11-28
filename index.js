b939ab076aca32cf39b754911311ca793b678854
i5ting <i5ting@126.com>
2016-11-28 16:40:49 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ module.exports = function (opts) {
     return
   }
   
-  var lines = []
   console.log('available script:')
 
   for (var _name in conf.scripts) {
     console.log('\t' + _name)
     var cmd = 'alias ' + _name + '=\'npm run ' + _name + '\''
-    lines.push(cmd)
   }
   
   var all = require(opts.root + '/static/task.json')
   
-  all.scripts = conf.scripts
+  all.npm_scripts = {
+    prefix: 'npm run',
+    tasks: conf.scripts
+  }
   
   fs.writeFileSync(opts.root + '/static/task.json',  JSON.stringify(all, null, 4))
 }
